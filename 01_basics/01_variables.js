3938ac2541034d485764d9d28c80d28fe559754d
Banajyoti Nath <145108158+loneconifer@users.noreply.github.com>
2024-12-24 12:09:56 +0000

    Basic variable concepts done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 1445522
+let accountEmail = "bana@google.com"
+var accountPassword = "52214"
+accountCity = "Rampur"
+
+// accountId = 2 // Not allowed
+
+accountEmail = "bn@jb.com"
+accountPassword = "21145"
+accountCity = "Palasbari"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
